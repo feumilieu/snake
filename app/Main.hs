1480bd9dcde7a634db698658c7e07c94ff597f5a
Aleksey Makarov <aleksey.makarov@auriga.com>
2015-11-18 17:00:21 +0300

    wip

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE TemplateHaskell #-}
 
--- TODO: eating rabbits and growing
 -- TODO: use state monad
+-- TODO: error when the snake eats himself
+-- TODO: meaningful messages when the snake eats border
 -- TODO: different speed when running vert vs horiz (?)
 
 module Main where
 
+import Data.Sequence hiding (zip, length)
 import Control.Monad
 import Control.Monad.IO.Class
 import System.Random
@@ -36,8 +38,8 @@ directionToChar DDown  = 'v'
 directionToChar DLeft  = '<'
 directionToChar DRight = '>'
 
-drawCharX :: (Integer, Integer) -> Char -> Update ()
-drawCharX (r, c) ch = do
+drawCharX :: Char -> (Integer, Integer) -> Update ()
+drawCharX ch (r, c) = do
     moveCursor r c
     drawGlyph $ Glyph ch []
 
@@ -60,7 +62,7 @@ newRabbit = do
     return (rr, rc)
 
 showRabbit :: (Integer, Integer) -> Update ()
-showRabbit r = drawCharX r '@'
+showRabbit = drawCharX '@'
 
 oops :: Maybe ColorID -> String -> Curses ()
 oops cid s = do
@@ -83,12 +85,22 @@ oops cid s = do
             closeWindow wnew
             render
 
-snakeRun :: (Integer, Integer) -> (Integer, Integer) -> Int -> Direction -> Curses ()
-snakeRun position r _ d = do
+firstBodyTail :: Seq a -> (a, Seq a, a)
+firstBodyTail s =
+    case viewl s of
+        EmptyL -> error "firstBodyTail: the length of the sequence is less than 1"
+        f :< tt ->
+            case viewr tt of
+                EmptyR -> error "firstBodyTail: the length of the sequence is less than 2"
+                b :> t -> (f, b, t)
+
+snakeRun :: Seq (Integer, Integer) -> (Integer, Integer) -> Int -> Direction -> Curses ()
+snakeRun s r g d = do
 
     w <- defaultWindow
+    (sfirst, sbody, slast) <- return $ firstBodyTail s
 
-    updateWindow w $ drawCharX position $ directionToChar d
+    updateWindow w $ drawCharX (directionToChar d) sfirst
     render
 
     ev <- getEvent w (Just 100)
@@ -100,32 +112,42 @@ snakeRun position r _ d = do
                 newDirection = case ev of
                     Just (EventSpecialKey k)  -> changeDirection k d
                     _ -> d
-                newPosition = move newDirection position
+                newPosition = move newDirection sfirst
                 in do
 
-                    nr <- if newPosition == r
+                    (nr, ng) <- if newPosition == r
                         then do
                             nr' <- newRabbit
-                            updateWindow w $ do
-                                drawCharX position ' '
-                                showRabbit nr'
-                            return nr'
+                            updateWindow w $ showRabbit nr'
+                            return (nr', g + 3)
                         else
-                            return r
+                            return (r, if g == 0 then 0 else g - 1)
 
-                    updateWindow w $ drawCharX position ' '
+                    updateWindow w $ do
+                        when (g == 0) $ drawCharX ' ' slast
+                        drawCharX snakeBodyChar sfirst
 
-                    snakeRun newPosition nr 0 newDirection
+                    snakeRun
+                        (newPosition <| sfirst <| (if (g == 0) then sbody else (sbody |> slast)))
+                        nr ng newDirection
+
+initialSnake :: Seq (Integer, Integer)
+initialSnake = fromList $ zip (repeat 0) [5, 4 .. 0]
+
+snakeBodyChar :: Char
+snakeBodyChar = '*'
 
 main :: IO ()
 main = runCurses $ do
     redColorId <- newColorIDX ColorRed ColorDefault 1
     rabbit <- newRabbit
     w <- defaultWindow
-    updateWindow w $ showRabbit rabbit
     ex <- tryCurses $ do
+        updateWindow w $ do
+            showRabbit rabbit
+            mapM_ (drawCharX snakeBodyChar) initialSnake
         void $ setCursorMode CursorInvisible
-        snakeRun (0, 0) rabbit 0 DRight
+        snakeRun initialSnake rabbit 0 DRight
     case ex of
         Right () -> return ()
         Left e -> oops redColorId $ show e
